a3e5975396e168a86ed87e6d576cef995c77bc96
HussBTWYT <121028340+HussBTWYT@users.noreply.github.com>
2023-12-10 10:41:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-import string
+import string 
 import random
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect # Importing the nessecary providers for Flask to use for our web app 
 
 app = Flask(__name__)
 
 ## characters to generate password from
 characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
+
+
 def generate_random_password(length):
     random.shuffle(characters)
     password = []
+    
     for i in range(length):
         password.append(random.choice(characters))
     random.shuffle(password)
+    
     return "".join(password)
 
 @app.route("/", methods=["GET", "POST"])
@@ -25,8 +29,11 @@ def index():
                 password = f'<p style="font-size: 30px;">Your randomly generated password is: {generate_random_password(length)}</p>'
         except ValueError:
             password = '<p style="color: red; font-size: 30px;">Result: Invalid input. Please enter a valid number.</p>'
-        
+
+    
     return render_template("index.html", password=password)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
